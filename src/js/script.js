0836d7fac42daf6d71b825138a839c16474a6707
Vremy <vincentremy99@hotmail.com>
2020-10-12 20:43:12 -0400

    avancement accueil et navigation mobile

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
     console.log("Ça fonctionne!!!");
 
 
+
+        document.querySelector("#menu").addEventListener("click", function (e) {
+
+
+            console.log("ouvert");
+
+            document.querySelector(".sousmenu").classList.add("open")
+            document.querySelector(".sousmenu").classList.remove("close")
+
+
+        }),
+
+
+
+            document.querySelector("#fermer").addEventListener("click", function (e) {
+
+
+                document.querySelector(".sousmenu").classList.add("close")
+                document.querySelector(".sousmenu").classList.remove("open")
+
+
+            })
+
+
+
 });
